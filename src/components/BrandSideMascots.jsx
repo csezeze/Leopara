204c0836cb0d59d4ff3f1e08485bdc0f21dc68f3
csezeze <csezeze@gmail.com>
2026-08-02 04:30:33 +0300

    polish mascot alignment and low score colors

--- src/components/BrandSideMascots.jsx
+++ src/components/BrandSideMascots.jsx
@@ -72,7 +72,6 @@ function BrandSideMascots() {
             height="512"
             draggable={false}
           />
-          <span className="brand-side-mascot-shirt-line" />
         </div>
         <div className="leopara-speech-bubble" role="note">
           <p>
